172394438f1d5c0d131deea2536d6622b5b600cd
Vyacheslav Malchik <for@validoll.ru>
2019-03-13 19:29:19 +0500

    Add functional test for token replacement

--- tests/src/Functional/FilterTest.php
+++ tests/src/Functional/FilterTest.php
@@ -3,6 +3,7 @@
 namespace Drupal\Tests\token_views_filter\Functional\Plugin;
 
 use Drupal\Tests\views\Functional\ViewTestBase;
+use Drupal\views\Views;
 
 /**
  * Tests token filter plugin functionality.
@@ -13,11 +14,18 @@ use Drupal\Tests\views\Functional\ViewTestBase;
 class FilterTest extends ViewTestBase {
 
   /**
+   * Views used by this test.
+   *
+   * @var array
+   */
+  public static $testViews = ['test_filter'];
+
+  /**
    * Modules to enable.
    *
    * @var array
    */
-  public static $modules = ['views_ui', 'node', 'token_views_filter'];
+  public static $modules = ['views_ui', 'token_views_filter'];
 
   protected function setUp($import_test_views = TRUE) {
     parent::setUp($import_test_views);
@@ -32,10 +40,27 @@ class FilterTest extends ViewTestBase {
    * Tests the settings form field exists.
    */
   public function testFormFieldUseTokenExists() {
-    $out = $this->drupalGet('admin/structure/views/nojs/handler/test_filter/default/filter/type');
+    $this->drupalGet('admin/structure/views/nojs/handler/test_filter/default/filter/type');
     // Make sure the field exists.
     $this->assertSession()
       ->fieldExists('options[use_tokens]');
+  }
+
+  /**
+   * Tests token replacement.
+   */
+  public function testTokenReplacement() {
+    $view = Views::getView('test_filter');
+    $view->initDisplay();
+
+    $filters = $view->display_handler->getOption('filters');
+    $filters['type']['value'] = '[site:name]';
+    $filters['type']['use_tokens'] = TRUE;
+
+    $view->display_handler->overrideOption('filters', $filters);
+    $this->executeView($view);
+
+    $this->assertSame('Drupal', $view->filter['type']->value);
   }
 
 }
